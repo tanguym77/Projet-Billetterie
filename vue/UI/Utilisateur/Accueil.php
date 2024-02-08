8b8b42c0653ebe0e53e547b4217321eb57c06440
tanguym77 <tanguy@marmier.me>
2024-02-08 13:45:01 +0100

    Mise à jour BP

--- vue/UI/Utilisateur/Accueil.php
+++ vue/UI/Utilisateur/Accueil.php
@@ -1,2 +1,17 @@
-PAGE Utilisateur
+<?php include './vue/UI/Utilisateur/Header.php'; ?>
     
+    <!-- CONTENU PAGE -->
+    <section>
+        <div class="row">
+            <div class="col-6 bg-info">
+                Text
+            </div>
+            <div class="col-6 bg-dark">
+                Image
+            </div>
+        </div>
+    </section>
+
+    <?php // include './vue/Footer.php'; ?>
+</body>
+</html>
--- vue/UI/Utilisateur/Header.php
+++ vue/UI/Utilisateur/Header.php
@@ -13,8 +13,8 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 
         <!-- JS -->
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
     
     <title>Billetterie</title>
 </head>
