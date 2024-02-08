54d504718c0acf27ad54ef4813e8b3d6c39f6a59
Wone Alexis <a.wone@student.isartdigital.com>
2024-02-08 14:18:07 +0100

    image test

--- vue/UI/Utilisateur/Accueil.php
+++ vue/UI/Utilisateur/Accueil.php
@@ -1,13 +1,85 @@
 <?php include './vue/UI/Utilisateur/Header.php'; ?>
     
     <!-- CONTENU PAGE -->
-    <section>
-        <div class="row">
-            <div class="col-6 bg-info">
-                Text
-            </div>
+    <section >
+        <div class="row m-0">
             <div class="col-6 bg-dark">
-                Image
+                <img src="./vue\image\arbre.jpg" class="img-fluid" alt="Image 1">
+            </div>
+            <div class="col-6 bg-info">
+                Text1
+            </div>
+        </div>
+        <div class="row m-0">
+            <div class="col-6 bg-dark">
+                <img src="./vue\image\arbre.jpg" class="img-fluid" alt="Image 1">
+            </div>
+            <div class="col-6 bg-info">
+                Text2
+            </div>
+        </div>
+        <div class="row m-0">
+            <div class="col-6 bg-dark">
+                <img src="./vue\image\arbre.jpg" class="img-fluid" alt="Image 1">
+            </div>
+            <div class="col-6 bg-info">
+                Text3
+            </div>
+        </div>
+        <div class="row m-0">
+            <div class="col-6 bg-dark">
+                <img src="./vue\image\arbre.jpg" class="img-fluid" alt="Image 1">
+            </div>
+            <div class="col-6 bg-info">
+                Text4
+            </div>
+        </div>
+        <div class="row m-0">
+            <div class="col-6 bg-dark">
+                <img src="./vue\image\arbre.jpg" class="img-fluid" alt="Image 1">
+            </div>
+            <div class="col-6 bg-info">
+                Text5
+            </div>
+        </div>
+        <div class="row m-0">
+            <div class="col-6 bg-dark">
+                <img src="./vue\image\arbre.jpg" class="img-fluid" alt="Image 1">
+            </div>
+            <div class="col-6 bg-info">
+                Text6
+            </div>
+        </div>
+        <div class="row m-0">
+            <div class="col-6 bg-dark">
+                <img src="./vue\image\arbre.jpg" class="img-fluid" alt="Image 1">
+            </div>
+            <div class="col-6 bg-info">
+                Text7
+            </div>
+        </div>
+        <div class="row m-0">
+            <div class="col-6 bg-dark">
+                <img src="./vue\image\arbre.jpg" class="img-fluid" alt="Image 1">
+            </div>
+            <div class="col-6 bg-info">
+                Text8
+            </div>
+        </div>
+        <div class="row m-0">
+            <div class="col-6 bg-dark">
+                <img src="./vue\image\arbre.jpg" class="img-fluid" alt="Image 1">
+            </div>
+            <div class="col-6 bg-info">
+                Text9
+            </div>
+        </div>
+        <div class="row m-0">
+            <div class="col-6 bg-dark">
+                <img src="./vue\image\arbre.jpg" class="img-fluid" alt="Image 1">
+            </div>
+            <div class="col-6 bg-info">
+                Text10
             </div>
         </div>
     </section>
