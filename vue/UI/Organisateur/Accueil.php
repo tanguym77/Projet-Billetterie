91456f0153d3e5c7c990f93412223630df94b7db
YussefBen <yussef.acm@gmail.com>
2024-02-08 15:03:10 +0100

    modif

--- vue/UI/Organisateur/Accueil.php
+++ vue/UI/Organisateur/Accueil.php
@@ -17,6 +17,11 @@
         .equipe-img {
             max-width: 50px;
             max-height: 50px;
+            margin-right: 10px;
+        }
+        .equipe-container {
+            display: flex;
+            align-items: center;
         }
     </style>
 </head>
@@ -27,11 +32,21 @@
             <div class="col-lg-4 col-md-6 mb-4">
                 <div class="card h-100">
                     <div class="card-body">
-                        <h4 class="card-title">Match 1</h4>
+                        <h4 class="card-title">Match 1 - Équipe A vs Équipe B</h4>
+                        <p class="card-text">Équipes: 
+                            <div class="equipe-container">
+                                <img class="equipe-img" src="vue/img/lsyrie.png" alt="Équipe A">
+                                <span>Équipe A</span>
+                            </div>
+                            <span>-</span>
+                            <div class="equipe-container">
+                                <img class="equipe-img" src="vue/img/chinel.png" alt="Équipe B">
+                                <span>Équipe B</span>
+                            </div>
+                        </p>
                         <p class="card-text">Date: 01/03/2024</p>
                         <p class="card-text">Heure: 20:00</p>
-                        <p class="card-text">Équipes: <img class="equipe-img" src="vue/img/lsyrie.png" alt="Équipe A"> - <img class="equipe-img" src="vue/img/chinel.png" alt="Équipe B"></p>
-                        <p class="card-text">Prix du billet: 50 €</p>
+                        <p class="card-text">Stade: Stade ...</p>
                         <button class="btn btn-primary">Modifier</button>
                         <button class="btn btn-danger">Supprimer</button>
                     </div>
@@ -40,11 +55,21 @@
             <div class="col-lg-4 col-md-6 mb-4">
                 <div class="card h-100">
                     <div class="card-body">
-                        <h4 class="card-title">Match 2</h4>
+                        <h4 class="card-title">Match 2 - Équipe C vs Équipe D</h4>
+                        <p class="card-text">Équipes: 
+                            <div class="equipe-container">
+                                <img class="equipe-img" src="vue/img/qatar.jpg" alt="Équipe C">
+                                <span>Équipe C</span>
+                            </div>
+                            <span>-</span>
+                            <div class="equipe-container">
+                                <img class="equipe-img" src="vue/img/jordan.png" alt="Équipe D">
+                                <span>Équipe D</span>
+                            </div>
+                        </p>
                         <p class="card-text">Date: 03/03/2024</p>
                         <p class="card-text">Heure: 18:00</p>
-                        <p class="card-text">Équipes: <img class="equipe-img" src="vue/img/qatar.png" alt="Équipe C"> - <img class="equipe-img" src="vue/img/japl.png" alt="Équipe D"></p>
-                        <p class="card-text">Prix du billet: 40 €</p>
+                        <p class="card-text">Stade: Stade ...</p>
                         <button class="btn btn-primary">Modifier</button>
                         <button class="btn btn-danger">Supprimer</button>
                     </div>
