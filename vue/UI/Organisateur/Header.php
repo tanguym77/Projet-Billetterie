4d0766e760bd25a835a9f08a21866d17b63d5417
YussefBen <yussef.acm@gmail.com>
2024-02-08 13:58:09 +0100

    modif header

--- vue/UI/Organisateur/Header.php
+++ vue/UI/Organisateur/Header.php
@@ -30,11 +30,11 @@
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <!-- LIEN 1 -->
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Lien 1</a>
+                    <a class="nav-link active" aria-current="page" href="#">Créer Ticket</a>
                 </li>
                 <!-- LIEN 2 -->
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Lien 2</a>
+                    <a class="nav-link" href="#">Utilisateurs</a>
                 </li>
                 <!-- LIEN 3 -->
                 <li class="nav-item">
