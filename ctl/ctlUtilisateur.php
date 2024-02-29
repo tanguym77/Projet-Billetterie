a4faac2da3ddf04d5ff49cab27aadd84773ec496
YussefBen <yussef.acm@gmail.com>
2024-02-29 11:03:56 +0100

    modif

--- ctl/ctlUtilisateur.php
+++ ctl/ctlUtilisateur.php
@@ -9,7 +9,6 @@ switch ($action) {
     case 'Accueil':
         // Récupération des infos users
         //$result = DbUtilisateur::list_ticket();
-        include './vue/UI/Utilisateur/Header.php';
         include './vue/UI/Utilisateur/Accueil.php';
         break;
 
