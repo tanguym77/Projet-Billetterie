208dfd19be7e0ebfeac1dc4636b48a3d16f0aba4
YussefBen <yussef.acm@gmail.com>
2024-02-29 14:04:08 +0100

    modifications utilisateur

--- model/DbUtilisateur.php
+++ model/DbUtilisateur.php
@@ -1,5 +1,19 @@
 <?php
 include './model/Connexion.php';
 
-	// Fonctions ...
+public static function searchUtilisateur($search)
+{
+	try {
+		$sql = "SELECT * FROM utilisateur WHERE (nom LIKE :search OR prenom LIKE :search OR email LIKE :search) AND status = 0";
+		$result = ModelPdo::$pdo->prepare($sql);
+		$result->bindValue(':search', $search);
+		$result->execute();
+		$liste = $result->fetchAll();
+		return $liste;
+	} catch (PDOException $e) {
+		echo $e->getMessage();
+		die("Erreur dans la BDD ");
+	}
+}
+
 ?>
