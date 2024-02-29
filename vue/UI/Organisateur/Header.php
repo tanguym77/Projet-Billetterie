f05f6231574ccbc8c9a44efd782a2c0f8643e5bc
YussefBen <yussef.acm@gmail.com>
2024-02-29 10:53:50 +0100

    modif

--- vue/UI/Organisateur/Header.php
+++ vue/UI/Organisateur/Header.php
@@ -30,7 +30,7 @@
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <!-- LIEN 1 -->
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Créer Ticket</a>
+                    <a class="nav-link active" aria-current="page" href="#">Créer Billet</a>
                 </li>
                 <!-- LIEN 2 -->
                 <li class="nav-item">
