b2dbd218528538b8328b3b94b922b9032082c093
Wone Alexis <a.wone@student.isartdigital.com>
2024-02-29 10:57:38 +0100

    acceuil slider

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ if(isset($_GET['ctl']))
             if(isset($_SESSION['status']) && $_SESSION['status'] == 1){
                 include 'ctl/ctlOrganisateur.php';
             }else{
+                include './vue/UI/Utilisateur/Header.php';
                 include './vue/UI/Utilisateur/Accueil.php';
             }
             break;
