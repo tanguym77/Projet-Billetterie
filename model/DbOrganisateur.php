0f7ae9c5f01edb75805a3f48a4806f60f33ac089
YussefBen <yussef.acm@gmail.com>
2024-03-01 10:36:31 +0100

    modif

--- model/DbOrganisateur.php
+++ model/DbOrganisateur.php
@@ -47,6 +47,81 @@ class DbOrganisateur{
 			$stmt->execute([$prix, $match_id]);
 		}
 	}
+
+
+
+	public static function infoUser($id)
+{
+	try {
+		$sql = "SELECT * FROM utilisateur WHERE id-utilisateur = :id";
+		$result = connectPdo::getObjPdo()->prepare($sql);
+		$result->bindValue(':id', $id);
+		$result->execute();
+		$liste = $result->fetchAll();
+		return $liste;
+	} catch (PDOException $e) {
+		echo $e->getMessage();
+		die("Erreur dans la BDD ");
+	}
+}
+
+public static function infoUserU($id)
+    {
+        try {
+            $sql = "SELECT * FROM utilisateur WHERE id_utilisateur = :id";
+            $result = connectPdo::getObjPdo()->prepare($sql);
+            $result->bindValue(':id', $id);
+            $result->execute();
+            $liste = $result->fetchAll();
+            return $liste;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            die("Erreur dans la BDD ");
+        }
+    }
+
+	public static function infoUserA($id)
+    {
+        try {
+            $sql = "SELECT * FROM utilisateur WHERE id_utilisateur = :id";
+            $result =connectPdo::getObjPdo()->prepare($sql);
+            $result->bindValue(':id', $id);
+            $result->execute();
+            $liste = $result->fetch();
+            return $liste;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            die("Erreur dans la BDD ");
+        }
+    }
+
+
+	public static function listeUserU()
+    {
+        try {
+            $sql = "SELECT * FROM utilisateur WHERE status = 0";
+            $result = connectPdo::getObjPdo()->query($sql);
+            $liste = $result->fetchAll();
+            return $liste;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            die("Erreur dans la BDD ");
+        }
+    }
+
+
+	public static function listeUserA()
+    {
+        try {
+            $sql = "SELECT * FROM utilisateur WHERE status = 1";
+            $result = connectPdo::getObjPdo()->query($sql);
+            $liste = $result->fetchAll();
+            return $liste;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            die("Erreur dans la BDD ");
+        }
+    }
 }
 
 ?>
