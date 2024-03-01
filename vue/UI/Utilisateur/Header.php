6d5475f49890804a9f3987dd259ed60580ce85f0
Wone Alexis <a.wone@student.isartdigital.com>
2024-03-01 08:48:18 +0100

    fix header lien + enleve div class inutile

--- vue/UI/Utilisateur/Header.php
+++ vue/UI/Utilisateur/Header.php
@@ -26,7 +26,7 @@
     <!-- NAVBAR -->
     <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
         <div class="container-fluid">
-            <a class="navbar-brand" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">GOAL !!!
+            <a class="navbar-brand" href="index.php">GOAL !!!
             <img src="vue/image/ballon-de-football.png"  alt="Logo"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
