d9b5f69b55656f50b972682a375767a8da51ba85
YussefBen <yussef.acm@gmail.com>
2024-03-14 09:50:26 +0100

    maj

--- vue/UI/Organisateur/vueEdituser.php
+++ vue/UI/Organisateur/vueEdituser.php
@@ -48,7 +48,7 @@
                     <div class="card bg-primary text-white"> <!-- Suppression de la classe "mb-3" pour réduire la taille -->
                         <div class="card-body">
                             <?php foreach ($infoUtilisateur as $info) { ?>
-                                <input type="hidden" name="id_utilisateur" value="<?php echo $info['mail']; ?>">
+                                <input type="hidden" name="Identifiant" value="<?php echo $info['mail']; ?>">
                                 <div class="mb-3">
                                     <label for="nom" class="form-label">Nom</label>
                                     <input type="text" class="form-control" id="nom" name="nom" value="<?php echo $info['nom']; ?>">
@@ -68,6 +68,9 @@
                                         <option value="0" <?php if ($info['status'] == '0') echo 'selected'; ?>>Utilisateur</option>
                                     </select>
                                 </div>
+                                <div class="mb-3">
+                                    <label for="mdp" class="form-label">Mot de passe</label>
+                                    <input type="password" class="form-control" id="password" name="password" placeholder="Mot de passe">
                             <?php } ?>
                             <button type="submit" class="btn btn-primary">Modifier</button>
                         </div>
