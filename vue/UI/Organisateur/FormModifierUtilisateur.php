170939574a9b4894afe1397d8da468ca0405ed0e
YussefBen <yussef.acm@gmail.com>
2024-03-21 10:42:18 +0100

    modif

--- vue/UI/Organisateur/FormModifierUtilisateur.php
+++ vue/UI/Organisateur/FormModifierUtilisateur.php
@@ -7,32 +7,31 @@
             <form action="index.php?ctl=Organisateur&action=ModifierUtilisateur" method="POST">
                 <div class="text-center row m-0 py-3">
                     <label class="col-6" for="nom">Nom </label>
-                    <input class="col-6" id="nom" name="nom" type="text" placeholder="Saisir un nom" value="<?php echo(['nom']) ?>">
+                    <!-- Utilisez les balises PHP correctement pour accéder aux valeurs -->
+                    <input class="col-6" id="nom" name="nom" type="text" placeholder="Saisir un nom" value="<?php echo $result['nom']; ?>">
                 </div>
 
                 <div class="text-center row m-0 py-3">
                     <label class="col-6" for="prenom">Prénom </label>
-                    <input class="col-6" id="prenom" name="prenom" type="text" placeholder="Saisir un prénom" value="<?php echo(['prenom']) ?>">
+                    <input class="col-6" id="prenom" name="prenom" type="text" placeholder="Saisir un prénom" value="<?php echo $result['prenom']; ?>">
                 </div>
 
                 <div class="text-center row m-0 py-3">
                     <label class="col-6" for="mail">Adresse Mail</label>
-                    <input class="col-6" id="mail" name="mail" type="number" placeholder="Saisir un mail" value="<?php echo(['mail']) ?>">
+                    <input class="col-6" id="mail" name="mail" type="number" placeholder="Saisir un mail" value="<?php echo $result['mail']; ?>">
                 </div>
 
 
                 <div class="text-center row m-0 py-3">
                     <label class="col-6" for="password">Mot de passe </label>
-                    <input class="col-6" id="password" name="password" type="text" placeholder="Saisir un mot de passe" value="<?php echo(['password']) ?>">
+                    <input class="col-6" id="password" name="password" type="text" placeholder="Saisir un mot de passe" value="<?php echo $result['password']; ?>">
                 </div>
 
 
-                <input type="hidden" name="id_utilisateur" value="<?php echo($_GET['id_utilisateur'])?>">
+                <input type="hidden" name="id_utilisateur" value="<?php echo $_GET['id_utilisateur']; ?>">
                 
 
-                <div class="text-center row m-0 py-3">
-                    <input class="col-12" id="capacite" type="submit">
-                </div>
+               
                 
             </form>
         </div>
