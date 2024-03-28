a78cff2955d3130c77b5fb37e1b58b8288e543d4
YussefBen <yussef.acm@gmail.com>
2024-03-28 10:16:18 +0100

    listStades affichage

--- vue/Organisateur/Stades/ListeStades.php
+++ vue/Organisateur/Stades/ListeStades.php
@@ -1,23 +1,39 @@
 <body>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+        }
 
-    <div class="row m-0 p-5">
+        .container {
+            padding: 20px;
+        }
 
+        .btn-retour {
+            margin-bottom: 10px;
+        }
 
+        .content {
+            margin-top: 10px;
+        }
+    </style>
+<body>
+
+<div class="container">
     <center>
-    <div class="btn-retour mt-2">
-        <a class="btn btn-secondary" href="javascript:history.go(-1)"><i class="fas fa-arrow-circle-left"></i> Retour </a>
-    </div>
+        <div class="btn-retour mt-2">
+            <a class="btn btn-secondary" href="javascript:history.go(-1)"><i class="fas fa-arrow-circle-left"></i> Retour</a>
+        </div>
     </center>
     
-    <span>
     <h2 class="my-4 text-center">Stades de football</h2>
-</span>
 
-        <div class="text-center py-3">
-            <a class="btn btn-primary" href="index.php?ctl=Organisateur&action=FormAjoutStade">Ajouter un stade</a>
-        </div>
+    <div class="text-center py-3">
+        <a class="btn btn-primary" href="index.php?ctl=Organisateur&action=FormAjoutStade">Ajouter un stade</a>
+    </div>
 
+    <div class="content">
         <?php for ($i=0; $i < count($result); $i++) { 
             echo'
             <div class="row border py-2 my-2">
@@ -34,7 +50,7 @@
             </div>
             ';
         } ?>
-        
-            
     </div>
+</div>
+
 </body>
